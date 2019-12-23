35079815a13283895f4726c65bd45511e5df64dc
Alexander Römer <alex@roemer.design>
2019-12-23 18:24:53 +0100

    Initial Commit

---
+++ LocalSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalSearch
+//
+//  Created by Alexander Römer on 23.12.19.
+//  Copyright © 2019 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
